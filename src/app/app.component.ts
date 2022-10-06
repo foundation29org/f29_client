1c6b8bd12bbcf4de50f9ef81979955e99a75b005
f29 javi <javier.logrono@outlook.com>
2022-10-06 12:53:28 +0200

    change seo titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,11 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap'
 import { Subscription } from 'rxjs/Subscription';
 import { Router, NavigationEnd, ActivatedRoute, NavigationStart, NavigationCancel } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { LangService } from 'app/shared/services/lang.service';
 import Swal from 'sweetalert2';
 import { EventsService } from 'app/shared/services/events.service';
-import { NgxHotjarService } from 'ngx-hotjar';
 
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 import {
@@ -38,7 +37,6 @@ export class AppComponent implements OnInit, OnDestroy {
   loggerSubscription: Subscription;
   actualPage: string = '';
   hasLocalLang: boolean = false;
-  actualScenarioHotjar: any = { lang: '', scenario: '' };
   tituloEvent: string = '';
   role: string = '';
   langs: any;
@@ -50,7 +48,7 @@ export class AppComponent implements OnInit, OnDestroy {
   private statusChangeSubscription: Subscription;
   private revokeChoiceSubscription: Subscription;
   private noCookieLawSubscription: Subscription;
-  constructor(private http: HttpClient, public toastr: ToastrService, private router: Router, private activatedRoute: ActivatedRoute, private titleService: Title, public translate: TranslateService, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics, private langService: LangService, private eventsService: EventsService, protected $hotjar: NgxHotjarService, private ccService: NgcCookieConsentService) {
+  constructor(private http: HttpClient, public toastr: ToastrService, private router: Router, private activatedRoute: ActivatedRoute, private titleService: Title, public translate: TranslateService, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics, private langService: LangService, private eventsService: EventsService, private ccService: NgcCookieConsentService, private meta: Meta) {
 
     if (sessionStorage.getItem('lang')) {
       this.translate.use(sessionStorage.getItem('lang'));
@@ -90,32 +88,15 @@ export class AppComponent implements OnInit, OnDestroy {
 
   }
 
-  launchHotjarTrigger(lang) {
-    this.actualScenarioHotjar.scenario = 'fake'
-    if (lang == 'es') {
-      var ojb = { lang: lang, scenario: 'generalincoming_es' };
-      this.testHotjarTrigger(ojb);
-    } else {
-      var ojb = { lang: lang, scenario: 'generalincoming_en' };
-      this.testHotjarTrigger(ojb);
-    }
-  }
-
-  testHotjarTrigger(obj) {
-    if (obj.scenario != this.actualScenarioHotjar.scenario) {
-      setTimeout(function () {
-        if (obj.lang == 'es') {
-          this.$hotjar.trigger(obj.scenario);
-          this.actualScenarioHotjar = obj
-        } else {
-          this.$hotjar.trigger(obj.scenario);
-          this.actualScenarioHotjar = obj
-        }
-      }.bind(this), 1000);
-    }
-  }
-
   ngOnInit() {
+
+    this.meta.addTags([
+      { name: 'keywords', content: this.translate.instant("seo.home.keywords") },
+      { name: 'description', content: this.translate.instant("seo.home.description") },
+      { name: 'title', content: this.translate.instant("seo.home.title") },
+      { name: 'robots', content: 'index, follow' }
+    ]);
+
     //evento que escucha si ha habido un error de conexión
     this.eventsService.on('http-error', function (error) {
       var msg1 = 'No internet connection';
@@ -166,27 +147,6 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     }.bind(this));
 
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        var actualUrl = this.activatedRoute.snapshot['_routerState'].url;
-        if (actualUrl.indexOf("undiagnosed;role=") != -1) {
-          this.role = actualUrl.split("undiagnosed;role=")[1];
-        } else if (actualUrl.indexOf("undiagnosed") != -1) {
-          this.role = "undiagnosed";
-        } else if (actualUrl.indexOf('diagnosed') != -1) {
-          this.role = "diagnosed";
-        } else {
-          this.role = '';
-        }
-
-        if (sessionStorage.getItem('lang') != undefined) {
-          this.launchHotjarTrigger(sessionStorage.getItem('lang'));
-        } else {
-          this.launchHotjarTrigger('en');
-        }
-      }
-    })
-
     this.subscription = this.router.events
       .filter((event) => event instanceof NavigationEnd)
       .map(() => this.activatedRoute)
@@ -202,6 +162,7 @@ export class AppComponent implements OnInit, OnDestroy {
           this.tituloEvent = event['title'];
           var titulo = this.translate.instant(this.tituloEvent);
           this.titleService.setTitle(titulo);
+          this.changeMeta();
         })();
 
         //para los anchor de la misma páginano hacer scroll hasta arriba
@@ -211,13 +172,15 @@ export class AppComponent implements OnInit, OnDestroy {
         this.actualPage = event['title'];
       });
 
+
     this.eventsService.on('changelang', function (lang) {
-      this.launchHotjarTrigger(lang);
+      
       (async () => {
         await this.delay(500);
         var titulo = this.translate.instant(this.tituloEvent);
         this.titleService.setTitle(titulo);
         sessionStorage.setItem('lang', lang);
+        this.changeMeta();
       })();
 
       this.translate
@@ -238,10 +201,6 @@ export class AppComponent implements OnInit, OnDestroy {
         this.ccService.init(this.ccService.getConfig()); // update config with translated messages
       });
 
-    }.bind(this));
-
-    this.eventsService.on('changeEscenarioHotjar', function (obj) {
-      this.testHotjarTrigger(obj);
     }.bind(this));
 
     this.ccService.getConfig().cookie.domain = window.location.hostname;
@@ -326,6 +285,23 @@ export class AppComponent implements OnInit, OnDestroy {
     this.statusChangeSubscription.unsubscribe();
     this.revokeChoiceSubscription.unsubscribe();
     this.noCookieLawSubscription.unsubscribe();
+  }
+
+  changeMeta() {
+    var URLactual = window.location.href;
+    if (URLactual.indexOf("donate") != -1) {
+      this.meta.updateTag({ name: 'keywords', content: this.translate.instant("seo.donate.keywords") });
+      this.meta.updateTag({ name: 'description', content: this.translate.instant("seo.donate.description") });
+      this.meta.updateTag({ name: 'title', content: this.translate.instant("seo.donate.title") });
+    } else if (URLactual.indexOf("news") != -1) {
+      this.meta.updateTag({ name: 'keywords', content: this.translate.instant("seo.news.keywords") });
+      this.meta.updateTag({ name: 'description', content: this.translate.instant("seo.news.description") });
+      this.meta.updateTag({ name: 'title', content: this.translate.instant("seo.news.title") });
+    } else {
+      this.meta.updateTag({ name: 'keywords', content: this.translate.instant("seo.home.keywords") });
+      this.meta.updateTag({ name: 'description', content: this.translate.instant("seo.home.description") });
+      this.meta.updateTag({ name: 'title', content: this.translate.instant("seo.home.title") });
+    } 
   }
 
 }
